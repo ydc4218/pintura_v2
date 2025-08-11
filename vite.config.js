bfccb3b575a46d998dd4b1d8adab04819defec7c
ydc4218 <79382659+ydc4218@users.noreply.github.com>
2025-08-11 16:47:34 -0500

    Versiones Iniciales

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'  // redirige /api a tu servidor express
+    }
+  }
 });
